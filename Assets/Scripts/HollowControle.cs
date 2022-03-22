0fedc4a0825c8771f26842ff46585d9fcb7178d6
Felipe Vieira Dantas <felipe@gmfgames.com>
2022-03-22 10:48:12 -0300

    Atualização hollow

--- Assets/Scripts/HollowControle.cs
+++ Assets/Scripts/HollowControle.cs
@@ -8,15 +8,38 @@ public class HollowControle : MonoBehaviour
     public float velocidade = 10;
     public float forcaPulo = 100;
 
+    SpriteRenderer desenhista;
+    Animator animacao;
+
+    public Transform posicaoDoPe;
+    public float raio = 0.2f;
+    public LayerMask oQueEhChao;
+    bool estamosNoChao;
+
     // Start is called before the first frame update
     void Start()
     {
         fisica = GetComponent<Rigidbody2D>();
+        desenhista = GetComponent<SpriteRenderer>();
+        animacao = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Verificar se está no chão
+        if(Physics2D.OverlapCircle(posicaoDoPe.position,
+            raio,oQueEhChao) == null)
+        {
+            estamosNoChao = false;
+        }
+        else
+        {
+            estamosNoChao = true;
+        }
+
+
+
         //Get Axis Horizontal:
         //retorna -1 se o jogador apertar para esquerda
         //retorna 1 se o jogador apertar para direita
@@ -32,9 +55,29 @@ public class HollowControle : MonoBehaviour
         //GetKeyDown retorna verdadeiro (true) se o jogador
         //acabou de apertar uma tecla. No caso, espaço
         bool pulei = Input.GetKeyDown(KeyCode.Space);
-        if (pulei == true)
+        if (pulei == true && estamosNoChao == true)
         {
             fisica.AddForce(new Vector2(0, forcaPulo));
         }   
+
+        //Flipar o personagem caso necessário
+        if(taApertando < 0)
+        {
+            desenhista.flipX = true;
+        }
+        else if(taApertando > 0)
+        {
+            desenhista.flipX = false;
+        }
+
+        //Animação
+        if(taApertando == 0)
+        {
+            animacao.SetBool("Andando", false);
+        }
+        else
+        {
+            animacao.SetBool("Andando", true);
+        }
     }
 }
